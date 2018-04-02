245ad80e820fb76f6395e03b198b18990f01276b
starlying <corp@siteserver.cn>
2018-04-02 12:32:09 +0800

    init

---
+++ Main.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using SiteServer.Plugin;
+using SiteServer.Plugin.Features;
+using SiteServer.Plugin.Models;
+
+namespace SiteServer.Crowler
+{ 
+    public class Main : PluginBase, IMenu
+    {
+        public override Func<int, PluginMenu> SiteMenu => GetMenu;
+
+        private static PluginMenu GetMenu(int publishmentSystemId)
+        {
+            return new PluginMenu
+            {
+                Text = "Web爬虫",
+                Href = "@/plugins/pageGatherRule.aspx"
+            };
+        }
+    }
+}
